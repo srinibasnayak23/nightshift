cae4422c467a6ef26ea59764dfaf244d81828681
Srinibas Nayak <srinibasnayak23@gmail.com>
2026-08-27 04:33:08 +0530

    production environment setup for frontend

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  wsBaseUrl: 'wss://nightshift-backend-r96j.onrender.com',
+  apiBaseUrl: 'https://nightshift-backend-r96j.onrender.com',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  wsBaseUrl: 'ws://localhost:8000',
+  apiBaseUrl: 'http://localhost:8000',
+};
